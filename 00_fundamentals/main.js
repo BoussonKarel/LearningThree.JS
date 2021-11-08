9322294dec53c82dff22296bf6bdb44bcc4b10bf
Karel <karel.bousson@student.howest.be>
2021-11-08 23:14:28 +0100

    updated fundamentals to responsive

--- 00_fundamentals/main.js
+++ 00_fundamentals/main.js
@@ -17,11 +17,11 @@ function main() {
   const scene = new THREE.Scene()
 
   {
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
-    scene.add(light);
+    const color = 0xffffff
+    const intensity = 1
+    const light = new THREE.DirectionalLight(color, intensity)
+    light.position.set(-1, 2, 4)
+    scene.add(light)
   }
 
   const boxWidth = 1
@@ -30,31 +30,53 @@ function main() {
   const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth)
 
   function makeInstance(geometry, color, x) {
-    const material = new THREE.MeshPhongMaterial({color});
+    const material = new THREE.MeshPhongMaterial({ color })
 
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    const cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
 
-    cube.position.x = x;
+    cube.position.x = x
 
-    return cube;
+    return cube
   }
 
   const cubes = [
-    makeInstance(geometry, 0x44aa88,  0),
+    makeInstance(geometry, 0x44aa88, 0),
     makeInstance(geometry, 0x8844aa, -2),
-    makeInstance(geometry, 0xaa8844,  2),
-  ];
+    makeInstance(geometry, 0xaa8844, 2),
+  ]
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement
+    const pixelRatio = window.devicePixelRatio
+    const width = (canvas.clientWidth * pixelRatio) | 0
+    const height = (canvas.clientHeight * pixelRatio) | 0
+    const needResize = canvas.width !== width || canvas.height !== height
+    if (needResize) {
+      renderer.setSize(width, height, false)
+    }
+    return needResize
+  }
 
   function render(time) {
     time *= 0.001 // convert time to seconds
 
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement
+      camera.aspect = canvas.clientWidth / canvas.clientHeight
+      camera.updateProjectionMatrix()
+    }
+
+    const canvas = renderer.domElement
+    camera.aspect = canvas.clientWidth / canvas.clientHeight
+    camera.updateProjectionMatrix()
+
     cubes.forEach((cube, ndx) => {
-      const speed = 1 + ndx * .1;
-      const rot = time * speed;
-      cube.rotation.x = rot;
-      cube.rotation.y = rot;
-    });
+      const speed = 1 + ndx * 0.1
+      const rot = time * speed
+      cube.rotation.x = rot
+      cube.rotation.y = rot
+    })
 
     renderer.render(scene, camera)
 
